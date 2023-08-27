aad70fcf3f4a18d60e72531daacdeeac8753d5ad
Ali <alisina_q@yahoo.com.au>
2023-08-27 17:15:45 +1000

    fix vpc issue in #1

--- 1_ec2-asg-launchtemplate-sg/main.tf
+++ 1_ec2-asg-launchtemplate-sg/main.tf
@@ -1,28 +1,42 @@
-data "aws_vpc" "vpc-72a7bf5b" {
-   id = "vpc-72a7bf5b"
-   /* filter {
-        name    = "tag:environment"
-        values  = ["home-lab"]
-    }  */
+resource "aws_vpc" "main" {
+    cidr_block = "10.1.0.0/16"
+
+    tags = {
+        Name        = "main"
+        Environment = "homelab"
+    }
 }
 
-data "aws_subnet" "subnet-31ee8c9e" {
-    vpc_id = data.aws_vpc.vpc-72a7bf5b.id
-    id = "subnet-31ee8c9e"
-   /* filter {
-        name    = "tag:environment"
-        values  = ["home-lab"]
-    } */
+resource "aws_subnet" "private_subnet" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = "10.1.1.0/24"
+    availability_zone   = var.region
 }
 
 resource "aws_security_group" "web_instance_sg" {
     name        = "web-server-security-group"
     description = "Allowing https requests only"
-    vpc_id      = data.aws_vpc.vpc-72a7bf5b.id
+    vpc_id      = aws_vpc.main.id
 
     tags = {
         Name = "web-server-security-group"
     }
+    
+    ingress {
+        description = "Allow inbound https traffic"
+        cidr_blocks = [aws_subnet.private_subnet.cidr_block]
+        from_port   = 443
+        protocol    = "tcp"
+        to_port     = 443
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }  
 }
 
 resource "aws_launch_template" "web_launch_template" {
@@ -33,7 +47,7 @@ resource "aws_launch_template" "web_launch_template" {
 }
 
 resource "aws_autoscaling_group" "asg" {
-    vpc_zone_identifier = [data.aws_subnet.subnet-31ee8c9e.id]
+    vpc_zone_identifier = [aws_subnet.private_subnet.id]
     desired_capacity = 2
     max_size = 3
     min_size = 1
